b367cd76734edcdb606cf8b52bb0fed65e55e0d2
AlexisNarvaez <alexiszed19@gmail.com>
2019-07-30 10:20:36 -0500

    Inicio

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel;
+
+
+  constructor() { }
+
+  ngOnInit() {
+
+    this.usuario = new UsuarioModel();
+
+    // this.usuario.email = 'alexisnarvaez97@hotmail.com';
+   }
+
+   Guardar( form: NgForm ){
+
+
+    if( form.invalid ) { return; }
+     console.log('Formulario enviado');
+     console.log(this.usuario);
+   }
+
+
+}
